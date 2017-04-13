aebc84ad79bc10e87b375939c9bb4f57aec7577c
Yuren Ju <yurenju@gmail.com>
2017-04-13 19:03:14 +0800

    move test cases in misc to pays

--- test/integration/leaves.test.js
+++ test/integration/leaves.test.js
@@ -3,24 +3,21 @@ const std = require('../../src/index');
 
 //WIP
 describe('休假', () => {
-    describe('特休', () => {
-        it('月薪 36,000, 平均時薪為 150 元', () => {
-            expect(std.hourlySalary(36000)).eq(150);
-        });
-    });
+  describe('特休', () => {
+  });
 
-    describe('事假', () => {
-    });
+  describe('事假', () => {
+  });
 
-    describe('產假', () => {
+  describe('產假', () => {
 
-    });
+  });
 
-    describe('陪產假', () => {
+  describe('陪產假', () => {
 
-    });
+  });
 
-    describe('病假', () => {
+  describe('病假', () => {
 
-    });
+  });
 });

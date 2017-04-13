86c787d2d8c0c6f00fd8ec36ec671893af236012
Conjuror <kuang.con@gmail.com>
2017-04-13 11:11:53 +0800

    test case category proposal

---
+++ test/leaves.case.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const std = require('../src/index');
+
+//WIP
+describe('休假', () => {
+    describe('特休', () => {
+        it('月薪 36,000, 平均時薪為 150 元', () => {
+            expect(std.hourlySalary(36000)).eq(150);
+        });
+    });
+
+    describe('事假', () => {
+    });
+
+    describe('產假', () => {
+
+    });
+
+    describe('陪產假', () => {
+
+    });
+
+    describe('病假', () => {
+
+    });
+});

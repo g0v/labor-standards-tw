f5e2b04dae631c998667496a5ef79a4d40c42c8d
Yuren Ju <yurenju@gmail.com>
2017-06-04 22:24:36 +0800

    polish retirement

--- features/step_definitions/retirement-steps.js
+++ features/step_definitions/retirement-steps.js
@@ -5,7 +5,8 @@ const std = require('../../src/index')
 
 defineSupportCode(function ({ Given, When, Then }) {
   When('驗證退休資格時', function () {
-    this.result = std.retire(this.age)
+    this.labor.onBoard(new Date(2017 - this.workYears, 6, 1))
+    this.result = this.labor.retire(new Date(2017, 6, 1))
   })
 
   Then('他 可 申請退休', function () {
